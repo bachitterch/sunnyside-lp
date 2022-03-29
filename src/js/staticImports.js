aa3a02378a0aebb4c5fdb2bc608e85b47078451f
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-03-29 00:29:32 -0700

    Change: Updated Readme file and fixed broken links

--- src/js/staticImports.js
+++ src/js/staticImports.js
@@ -1,11 +1,21 @@
 import logo from '../../public/images/logo.svg'
 import arrowDown from '../../public/images/icon-arrow-down.svg'
+// Services Images
 import transform from '../../public/images/image-transform.jpg'
 import standOut from '../../public/images/image-stand-out.jpg'
+
+// Testimonials Images
+import emilyR from '../../public/images/image-emily.jpg'
+import thomasS from '../../public/images/image-thomas.jpg'
+import jennieF from '../../public/images/image-jennie.jpg'
+
+// Gallery Images
 import milkBottles from '../../public/images/image-gallery-milkbottles.jpg'
 import orange from '../../public/images/image-gallery-orange.jpg'
 import iceCreamCone from '../../public/images/image-gallery-cone.jpg'
 import sugarCubes from '../../public/images/image-gallery-sugarcubes.jpg'
+
+// Footer Social Icons
 import iconFB from '../../public/images/icon-facebook.svg'
 import iconInsta from '../../public/images/icon-instagram.svg'
 import iconTw from '../../public/images/icon-twitter.svg'
@@ -15,6 +25,9 @@ document.querySelector('.header__logo').src = logo
 document.querySelector('.text__arrow_down').src = arrowDown
 document.querySelector('.transform__img').src = transform
 document.querySelector('.stand_out__img').src = standOut
+document.querySelector('#emily').src = emilyR
+document.querySelector('#thomas').src = thomasS
+document.querySelector('#jennie').src = jennieF
 document.querySelector('#milk-bottles').src = milkBottles
 document.querySelector('#orange').src = orange
 document.querySelector('#ice-cream-cone').src = iceCreamCone
