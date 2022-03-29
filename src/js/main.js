9be893b96aab2d051c5f926ac8207aafad336625
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-03-29 00:09:49 -0700

    Change: Added all sections (Project Complete)

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,8 @@
 import '../scss/style.scss'
-import logo from '../../public/images/logo.svg'
-import arrowDown from '../../public/images/icon-arrow-down.svg'
-import transform from '../../public/images/image-transform.jpg'
-import standOut from '../../public/images/image-stand-out.jpg'
-import milkBottles from '../../public/images/image-gallery-milkbottles.jpg'
-import orange from '../../public/images/image-gallery-orange.jpg'
-import iceCreamCone from '../../public/images/image-gallery-cone.jpg'
-import sugarCubes from '../../public/images/image-gallery-sugarcubes.jpg'
-
-document.querySelector('.header__logo').src = logo
-document.querySelector('.text__arrow_down').src = arrowDown
-document.querySelector('.transform__img').src = transform
-document.querySelector('.stand_out__img').src = standOut
-document.querySelector('#milk-bottles').src = milkBottles
-document.querySelector('#orange').src = orange
-document.querySelector('#ice-cream-cone').src = iceCreamCone
-document.querySelector('#sugar-cubes').src = sugarCubes
+import './staticImports'
 
 const Nav = document.querySelector('.mobile__nav')
 const NavToggle = document.querySelector('#navToggleBtn')
-const NavClose = document.querySelectorAll('fadeUp')
 
 NavToggle.addEventListener('click', () => {
 
---
+++ src/js/staticImports.js
@@ -0,0 +1,25 @@
+import logo from '../../public/images/logo.svg'
+import arrowDown from '../../public/images/icon-arrow-down.svg'
+import transform from '../../public/images/image-transform.jpg'
+import standOut from '../../public/images/image-stand-out.jpg'
+import milkBottles from '../../public/images/image-gallery-milkbottles.jpg'
+import orange from '../../public/images/image-gallery-orange.jpg'
+import iceCreamCone from '../../public/images/image-gallery-cone.jpg'
+import sugarCubes from '../../public/images/image-gallery-sugarcubes.jpg'
+import iconFB from '../../public/images/icon-facebook.svg'
+import iconInsta from '../../public/images/icon-instagram.svg'
+import iconTw from '../../public/images/icon-twitter.svg'
+import iconPi from '../../public/images/icon-pinterest.svg'
+
+document.querySelector('.header__logo').src = logo
+document.querySelector('.text__arrow_down').src = arrowDown
+document.querySelector('.transform__img').src = transform
+document.querySelector('.stand_out__img').src = standOut
+document.querySelector('#milk-bottles').src = milkBottles
+document.querySelector('#orange').src = orange
+document.querySelector('#ice-cream-cone').src = iceCreamCone
+document.querySelector('#sugar-cubes').src = sugarCubes
+document.querySelector('.social__icon_fb').src = iconFB
+document.querySelector('.social__icon_insta').src = iconInsta
+document.querySelector('.social__icon_tw').src = iconTw
+document.querySelector('.social__icon_pi').src = iconPi
